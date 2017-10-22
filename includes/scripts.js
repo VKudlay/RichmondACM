be618979c84322358b3c0242b7b620b830eefac1
VKudlay <32310964+VKudlay@users.noreply.github.com>
2017-10-22 15:58:11 -0400

    10/22 Update
    Incorporated non-functioning submenus (to be fully implemented later). Straightened out some code for readibility (to be furthered).

--- includes/scripts.js
+++ includes/scripts.js
@@ -1,22 +1,42 @@
 // Used by header function specifically
 $(document).ready(function(){
 	//Mobile optimization
+	var curr;
 
 	//(Mobile) shows and hides side navbar
-	$('.navbar-toggle').click(function() {
+	$('.navbar-toggle').click(function(){menu_in()});	
+	$("#cover").click(function(){menu_out()});
+
+	$(window).resize(function(){
+		if($(window).width() < 635) menu_out();
+	});
+
+
+	function menu_in(){
 		$(".nav").addClass("active");
 		$("#cover").show();
-	});	$('#cover').click(function() {
+	} function menu_out(){
 		$(".nav").removeClass("active");
 		$("#cover").hide();
-	});
+		$("ul.sub-menu").removeAttr("style");
+	}
 
 	//Selected menu button toggle + menu toggle
 	$(".nav li").click(function() {
+
 		$("iframe").attr("src", 
 			"pages/"+$(this).attr('id')+"/index.html");
 
 		$(".nav li").not($(this)).removeClass("selected");
-		$(this).addClass("selected");
+		if($(document).width() > 635)
+			$(this).addClass("selected");
+
+		if($(document).width() < 635 && $(this).hasClass("parent")){
+			$("ul.sub-menu").removeAttr("style");
+			$(this).find("ul.sub-menu").css("display", "grid");
+			$(this).find("ul.sub-menu").css("width", "200px");
+		} else {
+			$("ul.sub-menu").removeAttr("style");
+		}
 	});
 });
