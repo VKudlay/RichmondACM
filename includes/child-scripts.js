a4d4c7ad7804bd6aa22c31ec2a153d212dd9dcc4
VKudlay <32310964+VKudlay@users.noreply.github.com>
2017-10-30 17:24:05 -0400

    Revamp of Website structure
    Implements java injection to simulate independent header/footer

---
+++ includes/child-scripts.js
@@ -0,0 +1,5 @@
+$(document).ready(function(){
+// Used by header function specifically
+    $("#header").load("../../includes/child-header.html");
+    //$("#footer").load("footer.html");
+});
--- includes/scripts.js
+++ includes/scripts.js
@@ -1,42 +1,5 @@
 // Used by header function specifically
 $(document).ready(function(){
-	//Mobile optimization
-	var curr;
-
-	//(Mobile) shows and hides side navbar
-	$('.navbar-toggle').click(function(){menu_in()});	
-	$("#cover").click(function(){menu_out()});
-
-	$(window).resize(function(){
-		if($(window).width() < 635) menu_out();
-	});
-
-
-	function menu_in(){
-		$(".nav").addClass("active");
-		$("#cover").show();
-	} function menu_out(){
-		$(".nav").removeClass("active");
-		$("#cover").hide();
-		$("ul.sub-menu").removeAttr("style");
-	}
-
-	//Selected menu button toggle + menu toggle
-	$(".nav li").click(function() {
-
-		$("iframe").attr("src", 
-			"pages/"+$(this).attr('id')+"/index.html");
-
-		$(".nav li").not($(this)).removeClass("selected");
-		if($(document).width() > 635)
-			$(this).addClass("selected");
-
-		if($(document).width() < 635 && $(this).hasClass("parent")){
-			$("ul.sub-menu").removeAttr("style");
-			$(this).find("ul.sub-menu").css("display", "grid");
-			$(this).find("ul.sub-menu").css("width", "200px");
-		} else {
-			$("ul.sub-menu").removeAttr("style");
-		}
-	});
+    $("#header").load("includes/header.html");
+    //$("#footer").load("footer.html");
 });
